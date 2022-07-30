847b1369f26369b6d882a69dd16e7bdc2aa0b135
Mariela <mbonilla.guti@gmail.com>
2022-07-30 14:40:59 -0600

    add function searchAlajuela

--- src/test/java/bonilla/mariela/pagesCases/HomePageTest.java
+++ src/test/java/bonilla/mariela/pagesCases/HomePageTest.java
@@ -39,6 +39,25 @@ public class HomePageTest {
         adapter.findCssSelector((".property-item-title > strong")).click();
     }
 
+    public void searchAlajueja()throws IOException {
+        adapter.findElementById(("ProvinceId")).click();
+        adapter.findElementXpath(("//option[. = 'Alajuela']")).click();
+        adapter.findElementById(("CantonId")).click();
+        adapter.findElementById(("CantonId"));
+        adapter.findElementXpath(("//option[. = 'Alajuela']")).click();
+        adapter.findCssSelector((".btn-filter"));
+        adapter.findTagName(("body"));
+        adapter.findElementById(("DistrictId")).click();
+        adapter.findElementById(("PropertyTypeId")).click();
+        adapter.findElementById(("PropertyTypeId"));
+        adapter.findElementXpath(("//option[. = 'Terrenos']")).click();
+        adapter.findElementById(("MinPrice")).click();
+        adapter.findElementById(("MinPrice")).sendKeys("0");
+        adapter.findElementById(("MaxPrice")).click();
+        adapter.findElementById(("MaxPrice")).sendKeys("6000000000");
+        adapter.findCssSelector((".btn-filter")).click();
+        adapter.findCssSelector((".property-item-title > strong")).click();
+    }
 
     public void closeDriver() {
         adapter.closeDriver();
