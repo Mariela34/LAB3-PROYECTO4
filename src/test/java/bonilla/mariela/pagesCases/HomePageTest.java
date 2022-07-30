8203aaacd02fb55ce3dc6a9d30dfea145be7ebbc
Mariela <mbonilla.guti@gmail.com>
2022-07-30 14:53:18 -0600

    add function searchGuanacaste

--- src/test/java/bonilla/mariela/pagesCases/HomePageTest.java
+++ src/test/java/bonilla/mariela/pagesCases/HomePageTest.java
@@ -79,6 +79,25 @@ public class HomePageTest {
         adapter.findCssSelector((".property-item-title > strong")).click();
     }
 
+    public void searchGuanacaste()throws IOException {
+        adapter.findElementById(("ProvinceId")).click();
+        adapter.findElementXpath(("//option[. = 'Guanacaste']")).click();
+        adapter.findElementById(("CantonId")).click();
+        adapter.findElementById(("CantonId"));
+        adapter.findElementXpath(("//option[. = 'Carrillo']")).click();
+        adapter.findCssSelector((".btn-filter"));
+        adapter.findTagName(("body"));
+        adapter.findElementById(("PropertyTypeId")).click();
+        adapter.findElementById(("PropertyTypeId"));
+        adapter.findElementXpath(("//option[. = 'Finca']")).click();
+        adapter.findElementById(("MinPrice")).click();
+        adapter.findElementById(("MinPrice")).sendKeys("0");
+        adapter.findElementById(("MaxPrice")).click();
+        adapter.findElementById(("MaxPrice")).sendKeys("40000000");
+        adapter.findCssSelector((".btn-filter")).click();
+        adapter.findCssSelector((".property-item-title > strong")).click();
+    }
+
     public void closeDriver() {
         adapter.closeDriver();
     }
