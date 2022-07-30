90cea56b339b9e8c60bce6fe155961064ac06afb
Mariela <mbonilla.guti@gmail.com>
2022-07-30 14:17:06 -0600

    create Adapter class and add imports

---
+++ src/test/java/bonilla/mariela/adapter/Adapter.java
@@ -0,0 +1,13 @@
+package bonilla.mariela.adapter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.IOException;
+
+import java.time.Duration;
+
+
+public class Adapter {
+}
