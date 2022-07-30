fba34aa90d28b4144257b0bc025679860b3fbfa8
Mariela <mbonilla.guti@gmail.com>
2022-07-30 14:35:51 -0600

    add parameters to run test and test with seachSanJose test

--- src/test/java/bonilla/mariela/testcases/Cases.java
+++ src/test/java/bonilla/mariela/testcases/Cases.java
@@ -2,19 +2,38 @@ package bonilla.mariela.testcases;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import bonilla.mariela.pagesCases.HomePageTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.io.IOException;
+
+
 public class Cases
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    HomePageTest homePageTest;
+
+    @Parameters({"browser"})
+    @BeforeClass
+    public void beforeClass(String browser) {
+        homePageTest = new HomePageTest();
     }
+    @Parameters({"url"})
+    @BeforeMethod
+    public void beforeTest(String url) throws IOException {
+        homePageTest.openURL(url);
+    }
+
+    @Test
+    public void TestCpSanJose() throws IOException {
+        homePageTest.searchSanJose();
+    }
+
+
 }
+
