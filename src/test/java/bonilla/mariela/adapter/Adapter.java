4024d2b2ec0403317f70a153ef3513ca91d1fcc8
Mariela <mbonilla.guti@gmail.com>
2022-07-30 14:19:29 -0600

    fix empty line

--- src/test/java/bonilla/mariela/adapter/Adapter.java
+++ src/test/java/bonilla/mariela/adapter/Adapter.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.IOException;
-
 import java.time.Duration;
 
 
